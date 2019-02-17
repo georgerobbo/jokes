b8432bab72b64a601c510e8cdd6caf7edbaf9b4b
george <george.robertson3@hotmail.co.uk>
2019-02-17 22:14:58 +0000

    Topic 12

--- public/editjoke.php
+++ public/editjoke.php
@@ -6,17 +6,17 @@ require '../database.php';
 
 require '../functions.php';
 
-if (isset($_POST['joketext'])) {
-	$stmt = $pdo->prepare('UPDATE joke SET joketext = :joketext WHERE id = :id');
-	$stmt->execute(['joketext' => $_POST['joketext'], 'id' => $_POST['id']]);
-
-	header('location: jokes.php');
-}
-else {
-	$joke = find($pdo, 'joke', 'id', $_GET['id']);
-
-	$output = loadTemplate('../templates/editjoke.html.php', ['joke' => $joke[0]]);
-	$title = 'Edit joke';
+if (isset($_GET['id'])) {
+    $joke = find($pdo, 'joke', $_GET['id'], 'id'[0]);
+} else {
+    $joke = false;
 }
 
+if (isset($_POST['submit'])) {
+    save($pdo, 'joke', $_POST['joke'], 'id'[0]);
+    header('location: jokes.php');
+} else {
+    $output = loadTemplate('../templates/addjoke.html.php', ['joke' => $joke]);
+    $title = 'Add a new joke';
+}
 require  '../templates/layout.html.php';
